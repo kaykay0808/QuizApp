d3d90df554615cd90b267fa948553d75cdf34567
kay <kayshiro@live.no>
2021-05-12 00:01:57 +0200

    Some activity created

--- app/src/main/java/com/example/quizapp/Question.kt
+++ app/src/main/java/com/example/quizapp/Question.kt
@@ -1,4 +1,18 @@
 package com.example.quizapp
+//in this section we created a class with questions (right click, -> new -> kotlin file class -> class chosen)
 
-class Question {
-}
+// we added data before class and change out curve bracket to parantes
+data class Question (
+    val id: Int,
+    val question: String,
+    val image: Int
+
+    val optionOne: String,
+    val optionTwo: String,
+    val optionThree: String,
+    val optionFour: String,
+    val correctAnswer: Int
+
+
+
+)
