c0c9ffa5e7f24aa7bd46126315811d057722634e
kay <kayshiro@live.no>
2021-05-25 19:13:24 +0200

    98. done.
    Setting up the question UI and connecting the UI to the model

--- app/src/main/java/com/example/quizapp/QuizQuestionActivity.kt
+++ app/src/main/java/com/example/quizapp/QuizQuestionActivity.kt
@@ -3,14 +3,37 @@ package com.example.quizapp
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.example.quizapp.databinding.ActivityMainBinding
+import com.example.quizapp.databinding.ActivityQuizQuestionBinding
 
 class QuizQuestionActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityQuizQuestionBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_quiz_question)
+        binding = ActivityQuizQuestionBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val questionsList = Constants.getQuestions()
         // test check how many questions it is in this app
         Log.i("Questions Size", "${questionsList.size}")
+
+        val currentPosition = 1
+        val question: Question? = questionsList[currentPosition -1]
+
+        binding.progressBar.progress = currentPosition
+        binding.tvProgress.text ="$currentPosition" + "/" + binding.progressBar.max
+
+        binding.tvQuestion.text = question!!.question
+
+        // This is how we set image resources
+        binding.ivImage.setImageResource(question.image)
+
+        binding.tvOptionOne.text = question.optionOne
+        binding.tvOptionTwo.text = question.optionTwo
+        binding.tvOptionThree.text = question.optionThree
+        binding.tvOptionFour.text = question.optionFour
+
     }
 }
