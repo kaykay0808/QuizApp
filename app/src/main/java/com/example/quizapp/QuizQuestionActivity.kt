469464bff6e8fd91f1823fd4317f7ff4684848d8
kay <kayshiro@live.no>
2021-05-31 21:53:45 +0200

    99.1

--- app/src/main/java/com/example/quizapp/QuizQuestionActivity.kt
+++ app/src/main/java/com/example/quizapp/QuizQuestionActivity.kt
@@ -1,12 +1,22 @@
 package com.example.quizapp
 
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.example.quizapp.databinding.ActivityMainBinding
 import com.example.quizapp.databinding.ActivityQuizQuestionBinding
 
-class QuizQuestionActivity : AppCompatActivity() {
+class QuizQuestionActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var mCurrentPosition:Int = 1
+    private var mQuestionsList:ArrayList<Question>? = null
+    private var mSelectedOptionPosition: Int = 0
+
 
     private lateinit var binding: ActivityQuizQuestionBinding
 
@@ -15,25 +25,78 @@ class QuizQuestionActivity : AppCompatActivity() {
         binding = ActivityQuizQuestionBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val questionsList = Constants.getQuestions()
-        // test check how many questions it is in this app
-        Log.i("Questions Size", "${questionsList.size}")
 
-        val currentPosition = 1
-        val question: Question? = questionsList[currentPosition -1]
+        mQuestionsList = Constants.getQuestions()
 
-        binding.progressBar.progress = currentPosition
-        binding.tvProgress.text ="$currentPosition" + "/" + binding.progressBar.max
+        setQuestion()
+
+        binding.tvOptionOne.setOnClickListener(this)
+        binding.tvOptionTwo.setOnClickListener(this)
+        binding.tvOptionThree.setOnClickListener(this)
+        binding.tvOptionFour.setOnClickListener(this)
+
+    }
+
+    // Step 1: Create a function to set the question in the UI component
+    private fun setQuestion(){
+        mCurrentPosition = 1
+        val question = mQuestionsList!![mCurrentPosition-1] //<-- Getting the question from the list with the help of current position.
+
+        defaultOptionsView()
+
+        binding.progressBar.progress = mCurrentPosition
+        binding.tvProgress.text ="$mCurrentPosition" + "/" + binding.progressBar.max
 
         binding.tvQuestion.text = question!!.question
-
         // This is how we set image resources
-        binding.ivImage.setImageResource(question.image)
+        binding.ivImage.setImageResource(question.image) // <-- se over dette igjen
 
         binding.tvOptionOne.text = question.optionOne
         binding.tvOptionTwo.text = question.optionTwo
         binding.tvOptionThree.text = question.optionThree
         binding.tvOptionFour.text = question.optionFour
+    }
 
+    // function that makes it all default
+    private fun defaultOptionsView(){
+        val options = ArrayList<TextView>()
+
+        options.add(0, binding.tvOptionOne)
+        options.add(1, binding.tvOptionTwo)
+        options.add(2, binding.tvOptionThree)
+        options.add(3, binding.tvOptionFour)
+
+        for (option in options){
+            option.setTextColor(Color.parseColor("#7A8089"))
+            option.typeface = Typeface.DEFAULT
+            option.background = ContextCompat.getDrawable(this, R.drawable.default_option_border_bg)
+        }
+
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.tv_option_one ->{
+                selectedOptionView(binding.tvOptionOne, 1)
+            }
+            R.id.tv_option_two ->{
+                selectedOptionView(binding.tvOptionTwo, 2)
+            }
+            R.id.tv_option_three ->{
+                selectedOptionView(binding.tvOptionThree, 3)
+            }
+            R.id.tv_option_four ->{
+                selectedOptionView(binding.tvOptionFour, 4)
+            }
+        }
+    }
+
+    private fun selectedOptionView(tv: TextView, selectedOptionNumber: Int){
+        defaultOptionsView()
+        mSelectedOptionPosition = selectedOptionNumber
+
+        tv.setTextColor(Color.parseColor("#363A43"))
+        tv.setTypeface(tv.typeface, Typeface.BOLD)
+        tv.background = ContextCompat.getDrawable(this, R.drawable.selected_option_border_bg)
     }
 }
