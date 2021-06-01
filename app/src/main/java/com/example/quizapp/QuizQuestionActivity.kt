6abeed1e287e5cfbb5772060ae0c5498737f1bad
kay <kayshiro@live.no>
2021-06-01 22:11:47 +0200

    100

--- app/src/main/java/com/example/quizapp/QuizQuestionActivity.kt
+++ app/src/main/java/com/example/quizapp/QuizQuestionActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.example.quizapp.databinding.ActivityMainBinding
@@ -13,8 +14,11 @@ import com.example.quizapp.databinding.ActivityQuizQuestionBinding
 
 class QuizQuestionActivity : AppCompatActivity(), View.OnClickListener {
 
-    private var mCurrentPosition:Int = 1
-    private var mQuestionsList:ArrayList<Question>? = null
+    // Step 2: Create a member variables for current position and questions list.
+    private var mCurrentPosition: Int = 1 // Default and the first question position
+    private var mQuestionsList: ArrayList<Question>? = null
+
+    // Step 5: A global variables for selected option.
     private var mSelectedOptionPosition: Int = 0
 
 
@@ -30,22 +34,30 @@ class QuizQuestionActivity : AppCompatActivity(), View.OnClickListener {
 
         setQuestion()
 
+        // Step 4 set all the click events for Options using the interface onClick listener
         binding.tvOptionOne.setOnClickListener(this)
         binding.tvOptionTwo.setOnClickListener(this)
         binding.tvOptionThree.setOnClickListener(this)
         binding.tvOptionFour.setOnClickListener(this)
 
+        binding.btnSubmit.setOnClickListener(this)
+
     }
 
     // Step 1: Create a function to set the question in the UI component
-    private fun setQuestion(){
-        mCurrentPosition = 1
-        val question = mQuestionsList!![mCurrentPosition-1] //<-- Getting the question from the list with the help of current position.
+    private fun setQuestion() {
+        val question =
+            mQuestionsList!![mCurrentPosition - 1] //<-- Getting the question from the list with the help of current position.
 
         defaultOptionsView()
+        if (mCurrentPosition == mQuestionsList!!.size) {
+            binding.btnSubmit.text = "FINISH"
+        } else {
+            binding.btnSubmit.text = "SUBMIT"
+        }
 
         binding.progressBar.progress = mCurrentPosition
-        binding.tvProgress.text ="$mCurrentPosition" + "/" + binding.progressBar.max
+        binding.tvProgress.text = "$mCurrentPosition" + "/" + binding.progressBar.max
 
         binding.tvQuestion.text = question!!.question
         // This is how we set image resources
@@ -58,15 +70,16 @@ class QuizQuestionActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     // function that makes it all default
-    private fun defaultOptionsView(){
+    private fun defaultOptionsView() {
         val options = ArrayList<TextView>()
 
+        // Created an Array list from all the options textview
         options.add(0, binding.tvOptionOne)
         options.add(1, binding.tvOptionTwo)
         options.add(2, binding.tvOptionThree)
         options.add(3, binding.tvOptionFour)
 
-        for (option in options){
+        for (option in options) {
             option.setTextColor(Color.parseColor("#7A8089"))
             option.typeface = Typeface.DEFAULT
             option.background = ContextCompat.getDrawable(this, R.drawable.default_option_border_bg)
@@ -75,23 +88,82 @@ class QuizQuestionActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     override fun onClick(v: View?) {
-        when(v?.id){
-            R.id.tv_option_one ->{
+        when (v?.id) {
+            R.id.tv_option_one -> {
                 selectedOptionView(binding.tvOptionOne, 1)
             }
-            R.id.tv_option_two ->{
+            R.id.tv_option_two -> {
                 selectedOptionView(binding.tvOptionTwo, 2)
             }
-            R.id.tv_option_three ->{
+            R.id.tv_option_three -> {
                 selectedOptionView(binding.tvOptionThree, 3)
             }
-            R.id.tv_option_four ->{
+            R.id.tv_option_four -> {
                 selectedOptionView(binding.tvOptionFour, 4)
+            }
+            R.id.btn_submit -> {
+
+                if (mSelectedOptionPosition == 0) {
+
+                    mCurrentPosition++
+
+                    when {
+
+                        mCurrentPosition <= mQuestionsList!!.size -> {
+
+                            setQuestion()
+                        }
+                        else -> {
+                            Toast.makeText(
+                                this,
+                                "You have successfully completed the Quiz",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+                } else {
+                    val question = mQuestionsList?.get(mCurrentPosition - 1)
+
+                    // This will check if the answer is wrong
+                    if (question!!.correctAnswer != mSelectedOptionPosition) {
+                        answerView(mSelectedOptionPosition, R.drawable.wrong_option_border_bg)
+                    }
+
+                    // this is for correct answer
+                    answerView(question.correctAnswer, R.drawable.correct_option_border_bg)
+
+                    // if we are on the last question
+                    if (mCurrentPosition == mQuestionsList!!.size) {
+                        binding.btnSubmit.text = "FINISH"
+                    } else {
+                        binding.btnSubmit.text = "GO TO NEXT QUESTION"
+                    }
+
+                    mSelectedOptionPosition = 0
+                }
             }
         }
     }
 
-    private fun selectedOptionView(tv: TextView, selectedOptionNumber: Int){
+    private fun answerView(answer: Int, drawableView: Int) {
+        when (answer) {
+            1 -> {
+                binding.tvOptionOne.background = ContextCompat.getDrawable(this, drawableView)
+            }
+            2 -> {
+                binding.tvOptionTwo.background = ContextCompat.getDrawable(this, drawableView)
+            }
+            3 -> {
+                binding.tvOptionThree.background = ContextCompat.getDrawable(this, drawableView)
+            }
+            4 -> {
+                binding.tvOptionFour.background = ContextCompat.getDrawable(this, drawableView)
+            }
+        }
+
+    }
+
+    private fun selectedOptionView(tv: TextView, selectedOptionNumber: Int) {
         defaultOptionsView()
         mSelectedOptionPosition = selectedOptionNumber
 
